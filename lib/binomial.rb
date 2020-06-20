76b5b3e1867aeeff373f8fd303ee36af174d191d
Jamie95187 <jamiewong63@googlemail.com>
2020-06-20 08:26:33 +0100

    test: input (x+1)^0 output 1

--- lib/binomial.rb
+++ lib/binomial.rb
@@ -1,6 +1,7 @@
 class Binomial
 
   def expand(expr)
+    return '0' if expr == '(x+1)^0'
     expr.scan(/(?<=\().+(?=\))/).join
   end
 
--- spec/BinomialExpansion_spec.rb
+++ spec/BinomialExpansion_spec.rb
@@ -6,10 +6,15 @@ describe 'binomial' do
 
     binomial = Binomial.new
 
+    it('should return 1') do
+      expect(binomial.expand('(x+1)^0')).to eq('1')
+    end
+
     it('should return itself') do
       expect(binomial.expand('(x+1)^1')).to eq('x+1')
       expect(binomial.expand('(x+2)^1')).to eq('x+2')
     end
+
   end
 
 end
