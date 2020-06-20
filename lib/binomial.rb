7aad60484393117400efb3a3c1fd0460e917f7d8
Jamie95187 <jamiewong63@googlemail.com>
2020-06-20 08:47:56 +0100

    scanning degree of polynomial with scan

--- lib/binomial.rb
+++ lib/binomial.rb
@@ -1,6 +1,8 @@
 class Binomial
 
   def expand(expr)
+    degree = expr.scan(/(?<=\)).+/).join[1]
+    puts degree
     return '1' if expr.include?('^0')
     return 'x^2' if expr == '(x)^2'
     return 'x^2+2x+4' if expr == '(x+1)^2'
