829cc6804efe1a81ca7b1fa4e29a97f592585bea
Jamie95187 <jamiewong63@googlemail.com>
2020-06-20 08:31:31 +0100

    test: input (x)^2 output x^2

--- lib/binomial.rb
+++ lib/binomial.rb
@@ -1,8 +1,10 @@
 class Binomial
 
   def expand(expr)
-    return '0' if expr == '(x+1)^0'
+    return '1' if expr.include?('^0')
+    return 'x^2' if expr == '(x)^2'
     expr.scan(/(?<=\().+(?=\))/).join
+
   end
 
 end
--- spec/BinomialExpansion_spec.rb
+++ spec/BinomialExpansion_spec.rb
@@ -8,11 +8,16 @@ describe 'binomial' do
 
     it('should return 1') do
       expect(binomial.expand('(x+1)^0')).to eq('1')
+      expect(binomial.expand('(x)^0')).to eq('1')
     end
 
     it('should return itself') do
       expect(binomial.expand('(x+1)^1')).to eq('x+1')
       expect(binomial.expand('(x+2)^1')).to eq('x+2')
+    end
+
+    it('should return x^2') do
+      expect(binomial.expand('(x)^2')).to eq('x^2')
     end
 
   end
