c4402d8ef5944259d5b0530ff63a2775bd27d568
Jamie95187 <jamiewong63@googlemail.com>
2020-06-20 08:50:05 +0100

    refactor for degree 0

--- lib/binomial.rb
+++ lib/binomial.rb
@@ -2,8 +2,7 @@ class Binomial
 
   def expand(expr)
     degree = expr.scan(/(?<=\)).+/).join[1]
-    puts degree
-    return '1' if expr.include?('^0')
+    return '1' if degree == '0'
     return 'x^2' if expr == '(x)^2'
     return 'x^2+2x+4' if expr == '(x+1)^2'
     expr.scan(/(?<=\().+(?=\))/).join
